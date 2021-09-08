c69799e44cd6831ebde0c1c26f9c4eeffb30469a
ZolF-werk <zolf@bk.ru>
2021-09-08 18:53:13 +0300

    still_not_working

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,9 @@ import hStyle from '../Header/header.css';
 const Header = (props) => {
     console.log(hStyle);
     return (
-        <header className="root">
-            <div className={hStyle.forest}></div>
-            <div className={hStyle.container}>
+        <header class="root" className={hStyle.root}>
+            <div class="forest" className={hStyle.forest}></div>
+            <div class="container" className={hStyle.container}>
                 {props.title ? <h1>{props.title}</h1> : null}
 
                 <p>{props.descr}</p>
