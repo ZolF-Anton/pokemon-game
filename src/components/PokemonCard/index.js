bf034123fa5f930f7e4ab7e98f07b9171b67b52d
ZolF-werk <zolf@bk.ru>
2022-01-26 17:28:00 +0300

    Lesson_4

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,40 +1,30 @@
 import pStyle from './pokemonCard.module.css';
+import cn from 'classnames';
 import cardBackSide from './assets/card-back-side.jpg';
 import { useState } from 'react';
 
 const PokemonCard = (props) => {
-    const [isActive, setActive] = useState('false');
+    const [isActive, setActive] = useState(false);
     const onClickF = () => setActive(!isActive);
+    console.log(props.type);
 
     return (
         <div className={pStyle.root} onClick={onClickF}>
-            <div
-                className={`${pStyle.pokemonCard} ${
-                    isActive ? '' : pStyle.active
-                }`}
-            >
+            <div className={cn(pStyle.pokemonCard, { [pStyle.active]: isActive })}>
                 <div className={pStyle.cardFront}>
-                    <div className={`${pStyle.wrap} ${pStyle.front}`}>
-                        <div className={pStyle.pokemon}>
+                    <div className={cn(pStyle.wrap, pStyle.front)}>
+                        <div className={cn(pStyle.pokemon, pStyle[props.type])}>
                             <div className={pStyle.values}>
-                                <div
-                                    className={`${pStyle.count} ${pStyle.top}`}
-                                >
+                                <div className={cn(pStyle.count, pStyle.top)}>
                                     {props.values.top}
                                 </div>
-                                <div
-                                    className={`${pStyle.count} ${pStyle.right}`}
-                                >
+                                <div className={cn(pStyle.count, pStyle.right)}>
                                     {props.values.right}
                                 </div>
-                                <div
-                                    className={`${pStyle.count} ${pStyle.bottom}`}
-                                >
+                                <div className={cn(pStyle.count, pStyle.bottom)}>
                                     {props.values.bottom}
                                 </div>
-                                <div
-                                    className={`${pStyle.count} ${pStyle.left}`}
-                                >
+                                <div className={cn(pStyle.count, pStyle.left)}>
                                     {props.values.left}
                                 </div>
                             </div>
@@ -42,9 +32,7 @@ const PokemonCard = (props) => {
                                 <img src={props.img} alt={props.name} />
                             </div>
                             <div className={pStyle.info}>
-                                <span className={pStyle.number}>
-                                    #{props.id}
-                                </span>
+                                <span className={pStyle.number}>#{props.id}</span>
                                 <h3 className={pStyle.name}>{props.name}</h3>
                                 <small className={pStyle.type}>
                                     Type: <span>{props.type}</span>
@@ -56,7 +44,7 @@ const PokemonCard = (props) => {
 
                 <div className={pStyle.cardBack}>
                     <div className={`${pStyle.wrap} ${pStyle.back}`}>
-                        <img src={cardBackSide} alt='Сard Backed' />
+                        <img src={cardBackSide} alt="Сard Backed" />
                     </div>
                 </div>
             </div>
---
+++ src/routes/Game/index.js
@@ -0,0 +1,9 @@
+const GamePage = () => {
+    return (
+        <div>
+            <h2>This is game page</h2>
+        </div>
+    );
+};
+
+export default GamePage;
